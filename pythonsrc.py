32d60fb3884bf09495e51650ddf57f0ab2d66ff9
Anas Elghadi <anasgd2019@gmail.com>
2025-10-02 05:00:21 +0200

    added the__str__ method to the Maze class to visualize the maze grid.

--- pythonsrc.py
+++ pythonsrc.py
@@ -1,5 +1,5 @@
 from enum import Enum
-class cellstate(Enum):
+class CellState(Enum):
     path = 0
     wall = 1
     start = 2
@@ -8,18 +8,44 @@ class Maze :
     def __init__(self, width:int, length:int) -> None:
         self.width = width 
         self.length = length
-        self.grid = [[cellstate.path for _ in range(length)] for _ in range(width)]
+        self.grid = [[CellState.path for _ in range(length)] for _ in range(width)]
         self.start_pos = (0, 0) 
         self.end_pos = (width - 1, length - 1)
-        self.grid[0][0] = cellstate.start
-        self.grid[width-1][length-1] = cellstate.end
+        self.grid[0][0] = CellState.start
+        self.grid[width-1][length-1] = CellState.end
+    def get_grid(self):
+        return self.grid
+    
+    
     def get_cell_state(self, row:int,col:int):
+        if(row<0 or row>=self.width or col<0 or col>=self.length):
+            raise IndexError("Cell position out of bounds")
         return self.grid[row][col]
-    def set_cell_state(self, row:int,col:int,state:cellstate):
-        self.grid[row][col] = state
+    
+    
+    def set_cell_state(self, row:int,col:int,state:CellState)-> None:
+        
+        if(row<0 or row>=self.width or col<0 or col>=self.length) :
+            raise IndexError("Cell position out of bounds")
+        if(self.grid[row][col]!=CellState.start and self.grid[row][col]!=CellState.end):
+            self.grid[row][col] = state
+        elif state==CellState.start or state==CellState.end:
+            self.grid[row][col] = state
+            if state==CellState.start:
+                self.start_pos = (row,col)
+            else:
+                self.end_pos = (row,col)
+            
+        
+    def __str__(self):
+        maze_str = ""
+        for row  in self.grid:
+            maze_str += " ".join([str(cell.value) for cell in row]) + "\n"
+        return maze_str
 
 if __name__ == "__main__":
     maze = Maze(5,5)   
+    print(maze)
     # for i, row in enumerate(maze.grid):
        
     #     for cell in row:
